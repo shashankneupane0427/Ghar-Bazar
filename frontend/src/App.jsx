0f41eb22c878d84c18ca60113c046372e6580bf4
Shashank Neupane <suryaneupane0427@gmail.com>
2024-12-28 15:13:56 +0545

    updated footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
          <Route  path="/" element={<Home />}></Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { FaMailBulk, FaPhone } from 'react-icons/fa'
+import { FaLocationDot } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div>
-      foot
-    </div>
+    <footer>
+      <div className='max-padd-container flex items-start justify-between flex-col lg:flex-row gap-8 py-6 mb-7 bg-gradient-to-r from-primary via-white to-white '>
+        <div>
+          <h4 className='h4'>We are always here to help</h4>
+          <p>Find your dream property with ease. © Ghar Bazar, All Rights Reserved.</p>
+        </div>
+        <div className='flexStart flex-wrap gap-8'>
+          <div className='flexCenter gap-x-6'>
+            <FaLocationDot/>
+            <div>
+              <h5 className='h5'>Location</h5>
+                <p>123, Avenue, Cl</p>
+            </div>
+          </div>
+          <div className='flexCenter gap-x-6'>
+            <FaPhone/>
+            <div>
+              <h5 className='h5'>Phone</h5>
+                <p>+0123456789</p>
+            </div>
+          </div>
+          <div className='flexCenter gap-x-6'>
+            <FaMailBulk/>
+            <div>
+              <h5 className='h5'>Email Support</h5>
+                <p>info@gharbazar.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+
+    </footer>
   )
 }
 
