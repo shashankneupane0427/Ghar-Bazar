e852c92dc8bec4349d86cd0d8755da5632b0c6ae
Shashank Neupane <suryaneupane0427@gmail.com>
2024-12-28 14:46:56 +0545

    testimonial UI created

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+  import React from 'react'
+  import user1 from '../assets/testimonials/user1.png'
+  import {FaStar, FaCheck} from 'react-icons/fa'
+  import property1 from '../assets/img1.png'
+  import property2 from '../assets/img2.png'
+  import property3 from '../assets/img3.png'
+  import property4 from '../assets/img4.png'
 
-const Testimonials = () => {
-  return (
-    <div>
-      test
-    </div>
-  )
-}
+  const Testimonials = () => {
+    return (
+      <section className='max-padd-container bg-white py-16 xl:pt-28'>
+        {/* CONTAINER */}
+        <div className='grid grid-cols-1 lg:grid-cols-[2fr_5fr] gap-20 mb-16 rounded-2xl'>
+          {/* LEFT SIDE*/}
+          <div className='flex items-start justify-between flex-col gap-10'>
+            <h2 className='h2'>Customer feedback and experiences</h2>
+            <div className='flex flex-col gap-1 bg-white p-2 rounded ring-slate-900/5'>
+              <div className='flex text-secondary gap-2'> 
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>  
+              <div className='medium-14'>
+                 Showing 1-2 out of <b> 2k reviews </b>
+              </div>
+            </div>
+          </div>
+          {/* RIGHT SIDE*/}
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-7'>
+            {/* TESTIMONIAL CARD */}
+            <div className='rounded-lg p-4 bg-white ring-1 ring-slate-900/5'>
+              <div className='flexBetween'>
+                <div className='flexCenter gap-x-2'>
+                  <img src={user1} alt="userImg" height={44} width={44} className='rounded-full' />
+                  <h5 className='bold-14'>John Dow</h5>
+                </div>
+                <div className='bg-secondary text-white rounded-full flexCenter gap-x-2 p-1 px-2 text-xs font-semibold'> 
+                  <FaCheck/>
+                  Verified
+                </div> 
+              </div>
+              <hr className='h-[1px] w-full my-2' />
+              <div className='flex gap-x-1 text-secondary mt-5 mb-1 text-xs'>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <h4 className='h4'>High Quality</h4>
+              <p>Finding my dream home was so easy—great listings and excellent support!</p>
+              <div className='flex gap-x-1 mt-5'>
+                <img src={property1} alt="propertyImg" height={44} width={44} className='rounded aspect-square object-cover' />
+                <img src={property2} alt="propertyImg" height={44} width={44} className='rounded aspect-square object-cover' />
+              </div>
+            </div>
+            {/* TESTIMONIAL CARD */}
+            <div className='flex flex-col gap-1 rounded-lg p-4 bg-white ring-1 ring-slate-900/5'>
+              <div className='flexBetween'>
+                <div className='flexCenter gap-x-2'>
+                  <img src={user1} alt="userImg" height={44} width={44}className='rounded-full' />
+                  <h5 className='bold-14'>Izabella Stress</h5>
+                </div>
+                <div className='bg-secondary text-white rounded-full flexCenter gap-x-2 p-1 px-2 text-xs font-semibold'> 
+                  <FaCheck/>
+                  Verified
+                </div> 
+              </div>
+              <hr className='h-[1px] w-full my-2' />
+              <div className='flex gap-x-1 text-secondary mt-5 mb-1 text-xs'>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <h4 className='h4'>Modern Design</h4>
+              <p>Fantastic platform! Helped me find the perfect property quickly.</p>
+              <div className='flex gap-x-1 mt-5'>
+                <img src={property3} alt="propertyImg" height={44} width={44} className='rounded aspect-square object-cover' />
+                <img src={property4} alt="propertyImg" height={44} width={44} className='rounded aspect-square object-cover' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
 
-export default Testimonials
+  export default Testimonials
