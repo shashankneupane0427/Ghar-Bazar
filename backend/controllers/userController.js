c9980fc67cfb37381df0fd8ef228e29a194a46ad
Shashank Neupane <suryaneupane0427@gmail.com>
2024-12-30 13:27:49 +0545

    uupdate

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import asyncHandler from 'express-async-handler'
 import {prisma} from "../config/prismaConfig.js"
 
+
+// CONTROLLER FUNCTION FOR CREATING A USER
 export const createUser = asyncHandler(async (req, res) => {
     console.log("creating a user")
 
@@ -15,4 +17,49 @@ export const createUser = asyncHandler(async (req, res) => {
         })
     }
     else res.status(201).send({message:"User already registered"}) 
+})
+
+// CONTROLLER FUNCTION FOR BOOKING A RESIDENCY VISIT
+
+export const bookVisit = asyncHandler(async (req, res) => {
+    const {email, date} = req.body
+    const {id} = req.params
+
+    try {
+        const alreadyBooked = await prisma.user.findUnique({
+            where: {email: email},
+            select: {bookedVisits: true}
+        })
+
+        if(alreadyBooked.bookedVisits.some((visit)=>visit.id === id)) {
+            res.status(400).json({message: "This residency is already booked by you"})
+        }
+        else {
+            await prisma.user.update({
+                where: {email: email},
+                data: {
+                    bookedVisits: {push: {id, date}}
+                }
+            })
+            res.send("Your visit is booked successfully")
+        }
+    } catch (err) {
+        throw new Error(err.message)
+    }
+})
+
+// CONTROLLER FUNCTION FOR GETTING ALL BOOKINGS OF A USER
+
+export const allBookings = asyncHandler(async (req, res) => {
+     const {email}  = req.body
+     try {
+        const bookings = await prisma.user.findUnique({
+            where: {email},
+            select: {bookedVisits: true}
+        })
+        
+        res.status(200).send({bookings})
+     } catch(err) {
+        throw new Error(err.message)
+     }
 })
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {createUser} from "../controllers/userController.js"
+import {createUser, bookVisit, allBookings} from "../controllers/userController.js"
 
 const router = express.Router()
 
 router.post("/register", createUser)
+router.post("/bookVisit/:id", bookVisit)
+router.get("/allBookings", allBookings)
 
 export {router as userRoute}
